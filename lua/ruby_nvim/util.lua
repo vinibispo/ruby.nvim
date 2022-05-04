85ac7b03b64e2f12b1279bc583c77bee7b9a7099
vinibispo <vini.bispo015@gmail.com>
2022-05-04 01:01:26 -0300

    feat(util): add useful methods for helping with treesitter grammar

--- lua/ruby_nvim/util.lua
+++ lua/ruby_nvim/util.lua
@@ -24,6 +24,22 @@ local git_root = function(file)
   return job[1]
 end
 
+local function get_methods(root)
+  local keys = {}
+  for node, name in root:iter_children() do
+    if name == 'method' then
+      table.insert(keys, node)
+    end
+
+    if node:child_count() > 0 then
+      for _, child in pairs(get_methods(node)) do
+        table.insert(keys, child)
+      end
+    end
+  end
+  return keys
+end
+
 local split_path = function(path)
   local matches = {}
   for match in (path .. Path.path.sep):gmatch("(.-)" .. Path.path.sep) do
@@ -100,5 +116,52 @@ M.alternate = function(file_path)
     end
   end
 end
+
+M.all_methods = function ()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local ft = vim.api.nvim_buf_get_option(bufnr, "ft")
+  local tree = vim.treesitter.get_parser(bufnr, ft):parse()[1]
+  local root = tree:root()
+  return get_methods(root)
+end
+
+M.get_gem_name = function (node, bufnr)
+  while node ~= nil do
+    if node:type() == 'call' then
+      local method = node:field('method')[1]
+      local method_name = vim.treesitter.query.get_node_text(method, bufnr)
+      if method_name == 'gem' then
+        local method_args = node:field('arguments')[1] --that when using get_node_text has text 'aws-sdk-s3', '~> 1'
+        local gem_node_with_quotes = method_args:child(0) -- that when using get_node_text has text 'aws-sdk-s3'
+        local gem_node = gem_node_with_quotes:child(1) -- that when usin get_node_text has text aws-sdk-s3
+        local gem_name = vim.treesitter.query.get_node_text(gem_node, bufnr)
+        if gem_name ~= "'" then
+          return gem_name
+        end
+      end
+    end
+    node = node:parent()
+  end
+end
+
+M.get_method_relevant_to_cursor = function ()
+  local cursor_line = vim.api.nvim_win_get_cursor(0)[1]
+  local previous_node = nil
+  for _, node in pairs(M.all_methods()) do
+    local node_line, _ = node:start()
+    node_line = node_line + 1
+    if cursor_line == node_line then
+      return node
+    end
+
+    if cursor_line < node_line then
+      return previous_node
+    end
+
+    previous_node = node
+  end
+end
+
+
 
 return M

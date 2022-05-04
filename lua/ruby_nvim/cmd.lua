4f9bfc8ee34ca262bd31c3eac030652064683e59
vinibispo <vini.bispo015@gmail.com>
2022-05-04 15:47:18 -0300

    fix(messages): remove there is no gem message

--- lua/ruby_nvim/cmd.lua
+++ lua/ruby_nvim/cmd.lua
@@ -10,6 +10,7 @@ local schedule_wrap = vim.schedule_wrap
 local M = {}
 
 local no_file_or_not_rb_file = "buffer is empty or file is not a ruby file"
+local gem_not_found_error = "gem not found"
 
 local run_in_floating_window = function(cmd, args)
   local win = window.percentage_range_window(tonumber("0.8"), tonumber("0.8"))
@@ -86,7 +87,7 @@ end
 M.browse_gem = function()
   local node = util.get_method_relevant_to_cursor()
   if node == nil then
-    vim.notify("gem not found", "error")
+    vim.notify(gem_not_found_error, "error")
     return
   end
 
@@ -94,7 +95,7 @@ M.browse_gem = function()
 
   local gem_name = util.get_gem_name(node, bufnr)
   if gem_name == nil then
-    vim.notify("There is no gem in this line", "error")
+    vim.notify(gem_not_found_error, "error")
     return
   end
 

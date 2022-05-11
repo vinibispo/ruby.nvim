5c5bffe371241f84344d2a27ba712cea3fedfc76
vinibispo <vini.bispo015@gmail.com>
2022-05-11 10:00:58 -0300

    fix(rubyrun): add args in rubyrun as optional

--- ftplugin/ruby.lua
+++ ftplugin/ruby.lua
@@ -1,6 +1,6 @@
 local cmd = vim.cmd
 cmd(
-  [[command! -nargs=1 -complete=file RubyRun lua require("ruby_nvim.cmd").run(<f-args>)]])
+  [[command! -nargs=? -complete=file RubyRun lua require("ruby_nvim.cmd").run(<f-args>)]])
 cmd([[command! RubyAlternate lua require("ruby_nvim.cmd").alternate()]])
 cmd([[command! -bang RubyTest lua require("ruby_nvim.cmd").test("<bang>")]])
 cmd([[command! -nargs=0 RubyBrowseGem lua require('ruby_nvim.cmd').browse_gem()]])

1d7ef9214410f2b9c24fac9145f72767d7c47087
vinibispo <vini.bispo015@gmail.com>
2022-06-26 18:53:17 -0300

    feat(ruby_nvim): add new api for create_user_command and make user use at least neovim version 0.7
    # On branch feat/add-new-api-for-user-command
    # Changes to be committed:
    #   (use "git restore --staged <file>..." to unstage)
    # 	modified:   ftplugin/ruby.lua
    #
    # Changes not staged for commit:
    #   (use "git add <file>..." to update what will be committed)
    #   (use "git restore <file>..." to discard changes in working directory)
    # 	modified:   README.md
    #
    # Untracked files:
    #   (use "git add <file>..." to include in what will be committed)
    # 	doc/tags

--- ftplugin/ruby.lua
+++ ftplugin/ruby.lua
@@ -1,6 +1,29 @@
-local cmd = vim.cmd
-cmd(
-  [[command! -nargs=? -complete=file RubyRun lua require("ruby_nvim.cmd").run(<f-args>)]])
-cmd([[command! RubyAlternate lua require("ruby_nvim.cmd").alternate()]])
-cmd([[command! -bang RubyTest lua require("ruby_nvim.cmd").test("<bang>")]])
-cmd([[command! -nargs=0 RubyBrowseGem lua require('ruby_nvim.cmd').browse_gem()]])
+local ruby_cmd = require("ruby_nvim.cmd")
+VIM_VERSION_MINOR_SUPPORTED = 7
+if vim.version().minor < VIM_VERSION_MINOR_SUPPORTED then
+  vim.notify("Sorry, but ruby.nvim doesn't support minors less than " .. VIM_VERSION_MINOR_SUPPORTED, "error")
+else
+  vim.api.nvim_create_user_command("RubyRun", function(opts)
+    local arg
+    if opts.fargs ~= '' then
+      arg = opts.fargs[1]
+    else
+      arg = nil
+    end
+    ruby_cmd.run(arg)
+  end, { nargs = '?', complete = 'file' })
+
+  vim.api.nvim_create_user_command("RubyAlternate", function ()
+    ruby_cmd.alternate()
+  end, { nargs = 0 })
+
+  vim.api.nvim_create_user_command("RubyTest", function (opts)
+    ruby_cmd.test(opts.bang)
+  end, { bang = true })
+
+  vim.api.nvim_create_user_command("RubyBrowseGem", function ()
+    ruby_cmd.browse_gem()
+  end, {nargs = 0 })
+
+end
+
